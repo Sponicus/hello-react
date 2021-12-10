13f4cf44aa34f8c6c001d463ff4186cca6cbb147
Andrew <andrew.spon@gmail.com>
2021-12-10 18:29:20 -0500

    update app

--- src/index.js
+++ src/index.js
@@ -4,22 +4,26 @@ import './index.css';
 
 const Button = (props) => {
   // your code here
+  const [name, setName] = useState('');
+  const {reset} = props;
+  return (
+    <main>
+      <input onChange={(event) => setName(event.target.value)} placeholder='type your name'></input>
+      <button onClick={reset}>RESET</button>
+      <h1>Hello {name}</h1>
+    </main>
+  );
 };
 
 const Application = () => {
-
   // your code here
-
   const reset = () => {
     console.log("reset");
     // your code here
   };
 
   return (
-    <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
-    </main>
+    <Button reset={reset}/>
   );
 };
 
