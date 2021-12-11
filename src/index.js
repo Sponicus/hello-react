14b94e4f30d57fa6955e9f00fb8e2ed9c73563ce
Andrew <andrew.spon@gmail.com>
2021-12-11 12:52:38 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -4,26 +4,28 @@ import './index.css';
 
 const Button = (props) => {
   // your code here
-  const [name, setName] = useState('');
   const {reset} = props;
+  
   return (
-    <main>
-      <input onChange={(event) => setName(event.target.value)} placeholder='type your name'></input>
-      <button onClick={reset}>RESET</button>
-      <h1>Hello {name}</h1>
-    </main>
+    <button onClick={reset}>RESET</button>
   );
 };
 
 const Application = () => {
   // your code here
+  const [name, setName] = useState('');
   const reset = () => {
     console.log("reset");
     // your code here
+    setName("");
   };
 
   return (
-    <Button reset={reset}/>
+    <main>
+      <input onChange={(event) => setName(event.target.value)} placeholder='type your name' value={name}></input>
+      <Button reset={reset} />
+      { name && <h1>Hello {name}</h1>}
+    </main>
   );
 };
 
